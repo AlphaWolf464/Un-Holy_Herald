f8760c34e5cae2fe9b45f8cb602d801f7f5b9145
Lucas <73131037+TheShadowS1ayer@users.noreply.github.com>
2020-11-10 23:56:26 -0500

    Implements correct player movement, correct camera tracking, and a non-triggerable healthbar (WIP)

---
+++ Unholy Herald/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
