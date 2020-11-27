1e1d05d893b1fa7d7449357ee38ad2f557e3a0d1
Lucas <73131037+TheShadowS1ayer@users.noreply.github.com>
2020-11-27 17:25:51 -0500

    Fixed PlayerFaceMouse glitches
    Fixed the way that the PlayerFaceMouseScript determies what is a valid target to look at so that now it only rotates the avatar to look at "Geometry" or "Foe" gameobjects

--- Unholy Herald/Assets/myScripts/OnPlayer/PlayerFaceMouseScript.cs
+++ Unholy Herald/Assets/myScripts/OnPlayer/PlayerFaceMouseScript.cs
@@ -6,13 +6,17 @@ public class PlayerFaceMouseScript : MonoBehaviour
 {
     Ray cameraRay;
     RaycastHit cameraRayHit;
+    Vector3 mousePosition;
 
     void Update()
     {
         cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(cameraRay, out cameraRayHit))
         {
-            Vector3 mousePosition = new Vector3(cameraRayHit.point.x, transform.position.y, cameraRayHit.point.z);
+            if (cameraRayHit.transform.tag == "Geometry" || cameraRayHit.transform.root.tag == "Geometry" || cameraRayHit.transform.tag == "Foe" || cameraRayHit.transform.root.tag == "Foe")
+            {
+                mousePosition = new Vector3(cameraRayHit.point.x, transform.position.y, cameraRayHit.point.z);
+            }
             transform.LookAt(mousePosition);
         }
     }
